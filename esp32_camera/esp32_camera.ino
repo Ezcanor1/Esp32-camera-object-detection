8b78c59933a6fc0f3d713eb8826336c389ca73d5
Sany Das <107028023+Ezcanor1@users.noreply.github.com>
2025-09-28 00:17:30 +0530

    Add files via upload

--- esp32_camera/esp32_camera.ino
+++ esp32_camera/esp32_camera.ino
@@ -1,1 +1,24 @@
+void setup() {
+  // Initialize serial communication at 115200 baud rate
+  Serial.begin(115200);
+  
+  // Initialize built-in LED pin as output (usually GPIO 2)
+  pinMode(2, OUTPUT);
+  
+  // Wait for serial port to connect
+  delay(1000);
+  Serial.println("ESP32 Started!");
+  Serial.println("Built-in LED will blink every second");
+}
 
+void loop() {
+  // Turn LED on
+  digitalWrite(2, HIGH);
+  Serial.println("LED ON");
+  delay(1000);
+  
+  // Turn LED off
+  digitalWrite(2, LOW);
+  Serial.println("LED OFF");
+  delay(1000);
+}
